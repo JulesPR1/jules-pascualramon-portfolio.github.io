02594a8fcbe02a86b3a3f7e0e73e5d6d3c63bcfb
Jules PASCUAL-RAMON <jules.pascualramon.sio2020@gmail.com>
2023-10-03 15:56:50 +0200

    gitlab table

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,67 @@ $(window).scroll(function() {
     }
   });
 });
+
+var months = [
+  "Jan", "Fev", "Mar", "Avr", "Mai", "Juin", "Juil", "Aout", "Sep", "Oct", "Nov", "Dec"
+];
+
+var daysOfWeekShort = [
+  "Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"
+];
+
+var daysOfWeek = [
+  "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"
+];
+
+var today = new Date();
+var oneYearAgo = new Date();
+oneYearAgo.setFullYear(today.getFullYear() - 1);
+
+var currentMonth = -1;
+
+var weeks = Math.floor((today - oneYearAgo) / (7 * 24 * 60 * 60 * 1000));
+
+// Fetch data from the given URL
+$.ajax({
+  url: "https://git.isee-u.fr/users/jules/calendar.json",
+  dataType: "json",
+  success: function (data) {
+    for (var i = 0; i < weeks; i++) {
+      var weekDiv = $('<div class="week"></div>');
+      if (oneYearAgo.getMonth() !== currentMonth) {
+        currentMonth = oneYearAgo.getMonth();
+        weekDiv.append('<div class="month">' + months[currentMonth] + '</div>');
+      }
+      for (var j = 0; j < 7; j++) {
+        var dayDiv = $('<div class="day"></div>');
+        dayDiv.data("date", new Date(oneYearAgo));  // Store the date object as data
+        var formattedDate = oneYearAgo.toISOString().split('T')[0];
+        var commitCount = data[formattedDate] || 0;
+
+        if (commitCount === 0) {
+            dayDiv.addClass("gray");
+        } else if (commitCount >= 1 && commitCount <= 9) {
+            dayDiv.addClass("light-blue");
+        } else if (commitCount >= 10 && commitCount <= 19) {
+            dayDiv.addClass("medium-blue");
+        } else if (commitCount >= 20 && commitCount <= 29) {
+            dayDiv.addClass("dark-blue");
+        } else {
+            dayDiv.addClass("very-dark-blue");
+        }
+
+        var date = dayDiv.data("date");
+        var str = "Jour: " + daysOfWeek[date.getDay()] + ", Commits: " + commitCount;
+
+        // Add a tooltip
+        dayDiv.attr("title", str);
+
+
+        weekDiv.append(dayDiv);
+        oneYearAgo.setDate(oneYearAgo.getDate() + 1);
+      }
+      $('#weeks-container').append(weekDiv);
+    }
+  }
+});
